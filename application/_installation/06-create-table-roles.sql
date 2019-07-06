0a6a467d4958ab31ca72f74c254e886a979d52a7
Cristopher Castro <cristophernic@live.cl>
2019-07-06 18:00:23 -0400

    modificar nombres

--- application/_installation/06-create-table-roles.sql
+++ application/_installation/06-create-table-roles.sql
@@ -9,7 +9,7 @@ INSERT INTO `huge`.`roles` (`rol_id`, `rol_name`) VALUES
   (1, "Invitado"),
   (2, "Mesa central"),
   (3, "Profesional Ecografista"),
-  (4, "Jefe de institucion"),
+  (4, "Jefe de unidad ecográfica"),
   (5, "Vacio"),
   (6, "Vacio"),
   (7, "Administrador general");
