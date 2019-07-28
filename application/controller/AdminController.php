21ecc75945525dea4a5a20dee782a85d82ec3c47
Cristopher Castro <cristophernic@live.cl>
2019-07-28 13:07:29 -0400

    .

--- application/controller/AdminController.php
+++ application/controller/AdminController.php
@@ -5,8 +5,7 @@ class AdminController extends Controller
     /**
      * Construct this object by extending the basic Controller class
      */
-    public function __construct()
-    {
+    public function __construct(){
         parent::__construct();
         // special authentication check for the entire controller: Note the check-ADMIN-authentication!
         // All methods inside this controller are only accessible for admins (= users that have role type 7)
@@ -16,8 +15,7 @@ class AdminController extends Controller
     /**
      * This method controls what happens when you move to /admin or /admin/index in your app.
      */
-    public function index()
-    {
+    public function index(){
         $this->View->render('admin/index', array(
                 'users' => UserModel::getPublicProfilesOfAllUsers())
         );
